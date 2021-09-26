c1829fdfb3c7d0714013af172a29160a9951c280
张子枫 <1048411925@qq.com>
2021-09-26 23:33:05 +0800

    测试

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -561,7 +561,7 @@ async function plantBeanIndex() {
 }
 function readShareCode() {
   return new Promise(async resolve => {
-    $.get({url: `https://cdn.lu/api/bean/${randomCount}`, headers:{'Host':'api.jdsharecode.xyz'}, timeout: 10000}, (err, resp, data) => {
+    $.get({url: `https://cdn.nz.lu/api/bean/${randomCount}`, headers:{'Host':'api.jdsharecode.xyz'}, timeout: 10000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
